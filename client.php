1aaf0bc07e422df3c62e0e9ca280e5b8b4d50cc4
pjo <pjo@30e9dc4a-7611-4fbc-83ba-eecf79b76bd0>
2009-06-19 12:28:27 +0000

    now with license text
    git-svn-id: svn://oss.dbc.dk/sync_php/php/OpenLibrary/OpenSpell/trunk@4449 30e9dc4a-7611-4fbc-83ba-eecf79b76bd0

--- client.php
+++ client.php
@@ -1,4 +1,26 @@
 <?php
+
+/**                                                                                
+ *                                                                                 
+ * This file is part of OpenLibrary.                                               
+ * Copyright © 2009, Dansk Bibliotekscenter a/s,                                   
+ * Tempovej 7-11, DK-2750 Ballerup, Denmark. CVR: 15149043                         
+ *                                                                                 
+ * OpenLibrary is free software: you can redistribute it and/or modify             
+ * it under the terms of the GNU Affero General Public License as published by     
+ * the Free Software Foundation, either version 3 of the License, or               
+ * (at your option) any later version.                                             
+ *                                                                                 
+ * OpenLibrary is distributed in the hope that it will be useful,                  
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                  
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                   
+ * GNU Affero General Public License for more details.                             
+ *                                                                                 
+ * You should have received a copy of the GNU Affero General Public License        
+ * along with OpenLibrary.  If not, see <http://www.gnu.org/licenses/>.            
+*/                
+
+// diable caching of wsdl while developing
 ini_set('soap.wsdl_cache_enabled',0);
 if (! isset($_POST['submit']))
   {
